f7b0af6952f84671d129b5109b9d602f7519ef6c
王志森 <im.wongch1sum@gmail.com>
2022-09-01 15:19:47 +0800

    feat(basic-feature): 实现基础环境搭建，实现开发工具链的支持
    实现基础环境搭建，实现开发工具链的支持
    
    impl #1

---
+++ .umirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'dumi';
+export default defineConfig({
+  title: 'toy-ui',
+  favicon: '/assets/favicon.ico',
+  logo: '/assets/logo.png',
+  outputPath: 'docs-dist',
+  // more config: https://d.umijs.org/config
+});
